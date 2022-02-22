fc9b850d0c38b9b568220eefd415d77f995da941
EntireTwix <willkatz.dev@gmail.com>
2022-02-21 16:37:26 -0800

    added `to_string()` metaprogramming check

--- include/codectrl/can_to_string.h
+++ include/codectrl/can_to_string.h
@@ -2,8 +2,7 @@
 
 #include <iostream>
 
-// This is neccessary so we can check that a T at compile-time can be passed
-// into std::to_string.
+// This is neccessary so we can check that a T at compile-time can be passed into std::to_string.
 
 template <typename T>
 struct can_to_string : std::false_type {};
@@ -27,4 +26,23 @@ template <>
 struct can_to_string<long double> : std::true_type {};
 
 template <typename T>
-constexpr inline bool can_to_string_v = can_to_string<T>::value;
+inline constexpr bool can_to_string_v = can_to_string<T>::value;
+
+// This is neccessary so we can check that T at compile-time has the member function .to_string()
+
+template <typename C>
+struct has_to_string                                                                                                
+{                                                                                                                                         
+private:                                                                                                                                  
+    template <typename T>                                                                                     
+    static constexpr auto check(T *) -> typename std::is_same<decltype(std::declval<T>().to_string()), std::string>::type; 
+    template <typename>
+    static constexpr std::false_type check(...);                                                                                          
+    typedef decltype(check<C>(0)) type;                                                                                                   
+                                                                                                                                              
+public:                                                                                                                                   
+    static constexpr bool value = type::value;                                                                                            
+};
+
+template <typename T>
+inline constexpr bool has_to_string_v = has_to_string<T>::value; 
